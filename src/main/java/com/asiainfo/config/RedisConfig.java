fc016998c791aa6dba6676f1832edf0747aae5c5
hexin <450226399@qq.com>
2019-05-31 14:38:16 +0800

    KV-STORE数据源配置

--- src/main/java/com/asiainfo/config/RedisConfig.java
+++ src/main/java/com/asiainfo/config/RedisConfig.java
@@ -1,77 +1,42 @@
 package com.asiainfo.config;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.cache.annotation.EnableCaching;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
 
-@Component
-@Configuration
-@EnableCaching
-public class RedisConfig {
 
+@Configuration
+public class RedisConfig extends CachingConfigurerSupport {
+    private Logger log = LoggerFactory.getLogger(RedisConfig.class);
+
+    @Value("${spring.redis.host}")
     private String host;
 
+    @Value("${spring.redis.port}")
     private int port;
-
+    @Value("${spring.redis.password}")
     private String password;
 
-    private int timeout;
+    /*@Value("${spring.redis.time-out}")
+    private int timeout;*/
 
+    @Value("${spring.redis.maxActive}")
+    private int maxActive;
+
+    @Value("${spring.redis.maxIdle}")
     private int maxIdle;
 
+   /* @Value("${spring.redis.pool.min-idle}")
+    private int minIdle;*/
+
+    @Value("${spring.redis.maxWaitMillis}")
     private long maxWaitMillis;
-
-    public String getHost() {
-        return host;
-    }
-
-    public void setHost(String host) {
-        this.host = host;
-    }
-
-    public int getPort() {
-        return port;
-    }
-
-    public void setPort(int port) {
-        this.port = port;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public int getTimeout() {
-        return timeout;
-    }
-
-    public void setTimeout(int timeout) {
-        this.timeout = timeout;
-    }
-
-    public int getMaxIdle() {
-        return maxIdle;
-    }
-
-    public void setMaxIdle(int maxIdle) {
-        this.maxIdle = maxIdle;
-    }
-
-    public long getMaxWaitMillis() {
-        return maxWaitMillis;
-    }
-
-    public void setMaxWaitMillis(long maxWaitMillis) {
-        this.maxWaitMillis = maxWaitMillis;
-    }
 
 
 
@@ -80,8 +45,13 @@ public class RedisConfig {
 
         JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
         jedisPoolConfig.setMaxIdle(maxIdle);
+        jedisPoolConfig.setMaxTotal(maxActive);
         jedisPoolConfig.setMaxWaitMillis(maxWaitMillis);
-        JedisPool jedisPool = new JedisPool(jedisPoolConfig, host, port, timeout, password);
-        return jedisPool;
+        System.out.println("1231231");
+        JedisPool jedisPool = new JedisPool(jedisPoolConfig, host, port, 0, password);
+
+        log.debug("JedisPool注入成功！");
+       log.debug("redis地址：" + host + ":"+port+"pwd"+password );
+        return  jedisPool;
     }
 }
