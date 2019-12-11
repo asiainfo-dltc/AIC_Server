309f7647eae6390382012e75c89e9c1fbbfeb3a3
hexin <450226399@qq.com>
2019-12-11 11:12:55 +0800

    更新初始化数据

--- src/main/java/com/asiainfo/WhSpringBootApplication.java
+++ src/main/java/com/asiainfo/WhSpringBootApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WhSpringBootApplication {
 
 	public static void main(String[] args) {
+
+		System.setProperty("es.set.netty.runtime.available.processors","false");
 		SpringApplication.run(WhSpringBootApplication.class, args);
 	}
 }
